2afe835188dfa4d415a4b279813af0cd0e386351
djgnfj-svg <djgnfj8923@naver.com>
2021-12-26 14:21:50 +0900

    ADD model Users e.g

--- Reviewer/models.py
+++ Reviewer/models.py
@@ -1,3 +1,48 @@
-from django.db import models
+import string
+import random
 
+from django.db import models
+from django.contrib.auth.models import User as U
 # Create your models here.
+
+class TimeStampedModel(models.Model):
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class PayPlan(TimeStampedModel):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+
+
+class Organization(TimeStampedModel):
+    name = models.CharField(max_length=50)
+    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING, null=True)
+
+class Users(models.Model):
+    user = models.OneToOneField(U, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100, null=True)
+    organization = models.ForeignKey(Organization, on_delete=models.DO_NOTHING, null=True)
+
+class Categories(TimeStampedModel):
+    name = models.CharField(max_length=100)
+    organization = models.ForeignKey(Organization, on_delete=models.DO_NOTHING, null=True)
+    creator = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+class StudyList(TimeStampedModel):
+    def rand_string():
+        str_pool = string.digits + string.ascii_letters
+        return ("".join([random.choice(str_pool) for _ in range(6)])).lower()
+        
+    def rand_letter():
+        str_pool = string.ascii_letters
+        return random.choice(str_pool).lower()
+
+    nick_name = models.CharField(max_length=100)
+    category = models.ForeignKey(Categories, on_delete=models.DO_NOTHING, null=True)
+    creator = models.ForeignKey(Users, on_delete=models.CASCADE)    
+    study_topic = models.CharField(max_length=30)
+    study_contect = models.TextField()
--- Reviewer/views.py
+++ Reviewer/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect, render
 from Reviewer.form import LoginForm
-
+from Reviewer.models import Users
 # Create your views here.
 
 
-def login_view(requset):
-	if requset.method == "POST":
-		
+def login_view(request):
+	msg = None
+	if request.method == "POST":
+		form = LoginForm(request.POST)
+		if form.is_valid():
+			email = form.cleaned_data.get("email")
+			raw_password = form.cleaned_data.get("password")
+			mag = "가입되잇지 않거나 키보드를 사야할지도 모릅니다."
+		try :
+			user = Users.object.get(email=email)	
+		except Users.DoesNotExist:
+			pass
+		else :
+			if user.check_password(raw_password):
+				msg = None
+				login(request, user)
 	else:
 		form = LoginForm()
-	return render(requset, "login.html", {"form" : form})
+	return render(request, "login.html", {"form" : form, "msg" : msg})
+
+def logout_view(request):
+	logout(request)
+	return redirect("")
