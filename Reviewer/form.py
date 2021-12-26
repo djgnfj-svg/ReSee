ece89c929317310c8ef3640e9cf82a7552251e3a
djgnfj-svg <djgnfj8923@naver.com>
2021-12-26 15:36:10 +0900

    upload temp_file register

--- Reviewer/form.py
+++ Reviewer/form.py
@@ -1,13 +1,34 @@
 from django import forms
-from django.forms.widgets import PasswordInput
+from django.contrib.auth.forms import UserCreationForm
 
+from Reviewer.models import Users
 
 
 class LoginForm(forms.Form):
-	email = forms.CharField(
-		max_length=100, required=True, widget=forms.TextInput(attrs={"class" : "input-field", "placeholder": "이메일"})
-	)
-	password = forms.CharField(
-		max_length=30, required=True, 
-		widget=forms.PasswordInput(attrs={"class" : "input-field", "placeholder": "패스워드"})
-	)
+    email = forms.EmailField(
+        max_length=100, required=True, widget=forms.TextInput(attrs={"class" : "input-field", "placeholder": "이메일"})
+    )
+    password = forms.CharField(
+        max_length=30, required=True, 
+        widget=forms.PasswordInput(attrs={"class" : "input-field", "placeholder": "패스워드"})
+    )
+
+
+class RegisterForm(UserCreationForm):
+    username = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={"class" : "input-field", "placeholder": "유저이름"}))
+    password = forms.CharField(
+        max_length=30, required=True, 
+        widget=forms.PasswordInput(attrs={"class" : "input-field", "placeholder": "패스워드"})
+    )
+    email = forms.EmailField(
+        max_length=100, required=True, widget=forms.TextInput(attrs={"class" : "input-field", "placeholder": "이메일"})
+    )
+
+    class Meta:
+        model = Users
+        fields=(
+        "username",
+        "email",
+        "password1",
+        "password2",
+        )
--- Reviewer/views.py
+++ Reviewer/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
-from Reviewer.form import LoginForm
+from Reviewer.form import LoginForm, RegisterForm
 from Reviewer.models import Users
 from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
@@ -9,6 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 def login_view(request):
     if request.method == "POST":
         form = LoginForm(request.POST)
+        form_register = RegisterForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data.get("email")
             raw_password = form.cleaned_data.get("password")
@@ -24,8 +25,24 @@ def login_view(request):
     else:
         msg = None
         form = LoginForm()
-    print(msg)
+    print(request.id)
     return render(request, "login.html", {"form" : form, "msg" : msg})
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        msg = "잘쳐라 씹라야"
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            msg = "가입완료"
+        return render(request, "login.html", {"form_register": form, "msg": msg})
+    else:
+        form = RegisterForm()
+    return render(request, "login.html", {"form_register": form})
 
 def logout_view(request):
     logout(request)
